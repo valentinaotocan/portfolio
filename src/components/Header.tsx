bbd6acf96b485e44f20d5a89fd9f5e2dfc8b52e8
Valentina Otočan <valentinaotocan@gmail.com>
2023-07-16 19:23:44 +0200

    Active and non-active style for links in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import { NavLink } from 'react-router-dom';
 function Header() {
+  const isActive = true; 
   return (
-    <nav className='header ptb-xl txt-color-secondary fw-medium'>
-      <NavLink to="/">Naslovna</NavLink>
-      <NavLink to="/o-meni">O meni</NavLink>
+    <nav className="header ptb-xl txt-color-secondary fw-medium">
+      <NavLink to="/" className="mr-xl underline">
+        Naslovna
+      </NavLink>
+      <NavLink to="/o-meni" className="mr-xl underline">
+        O meni
+      </NavLink>
     </nav>
   );
 }
