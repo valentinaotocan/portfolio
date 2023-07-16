9fa06a6b73b08ac71a199433be76acb3223c2194
Valentina Otočan <valentinaotocan@gmail.com>
2023-07-16 22:46:43 +0200

    Add NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header';
+import NotFound from './pages/NotFound';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/o-meni" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div>
+      <p>Uuuups, dogodila se greška!</p>
+      <p>Server ne može pronaći traženo.</p>
+    </div>
+  )
+}
+export default NotFound
