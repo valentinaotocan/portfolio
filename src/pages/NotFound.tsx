10200281ea8eb58d18b749a101eeaba5441d2046
Valentina Otočan <valentinaotocan@gmail.com>
2023-07-17 10:22:06 +0200

    Style for NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,10 @@
 function NotFound() {
   return (
-    <div>
-      <p>Uuuups, dogodila se greška!</p>
+    <div className="not-found txt-color-secondary">
+      
+      <p className="fs-xl">Uuuups, dogodila se greška!</p>
       <p>Server ne može pronaći traženo.</p>
     </div>
-  )
+  );
 }
 export default NotFound
