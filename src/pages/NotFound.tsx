2216df2df8ba805551069946b1342dead8f718b9
Valentina Otočan <valentinaotocan@gmail.com>
2023-07-17 12:09:38 +0200

    Add error-404 image and style it

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,8 @@
+import error from '../assets/images/icons/error-404.png';
 function NotFound() {
   return (
-    <div className="not-found txt-color-secondary">
-      
+    <div className="not-found">
+      <img src={error} alt="Error" />
       <p className="fs-xl">Uuuups, dogodila se greška!</p>
       <p>Server ne može pronaći traženo.</p>
     </div>
