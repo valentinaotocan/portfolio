591ec5ca33822cd69d2b6e923bfdf30399763c1e
Valentina Otočan <valentinaotocan@gmail.com>
2023-07-26 11:34:05 +0200

    Create footer component and style it

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer linear-bg plr-custom txt-color-primary fs-base fw-normal">
+      <p>Sva prava pridržana. &copy; Valentina Otočan {new Date().getFullYear()}.</p>
+    </footer>
+  )
+}
