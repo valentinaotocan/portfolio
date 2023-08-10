c644a609b15cae7dfdfc03ad765c95573c875d15
Valentina Otočan <valentinaotocan@gmail.com>
2023-08-10 23:17:38 +0200

    Responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,45 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { IoCloseSharp, IoReorderThreeSharp } from "react-icons/io5";
+
 function Header() {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
   const isActive = true; 
+
+  const toggleMenu = () => {
+    setIsMenuOpened(!isMenuOpened);
+  };
+
   return (
     <nav className="header ptb-xl txt-color-secondary fw-medium sans-serif">
-      <NavLink to="/" className="mr-xl underline">
-        Naslovna
-      </NavLink>
-      <NavLink to="/o-meni" className="mr-xl underline">
-        O meni
-      </NavLink>
-      <NavLink to="/iskustvo" className="mr-xl underline">
-        Iskustvo
-      </NavLink>
-      <NavLink to="/projekti" className="mr-xl underline">
-        Projekti
-      </NavLink>
-      <NavLink to="/kontakt" className="mr-xl underline">
-        Kontakt
-      </NavLink>
+      <div
+        className={`header__nav ${isMenuOpened ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <NavLink to="/" className="mr-xl underline">
+          Naslovna
+        </NavLink>
+        <NavLink to="/o-meni" className="mr-xl underline">
+          O meni
+        </NavLink>
+        <NavLink to="/iskustvo" className="mr-xl underline">
+          Iskustvo
+        </NavLink>
+        <NavLink to="/projekti" className="mr-xl underline">
+          Projekti
+        </NavLink>
+        <NavLink to="/kontakt" className="mr-xl underline">
+          Kontakt
+        </NavLink>
+      </div>
+      <div className="header__hamburger" onClick={toggleMenu}>
+        {isMenuOpened ? (
+          <IoCloseSharp size={50} className="txt-color-secondary" />
+        ) : (
+          <IoReorderThreeSharp size={50} className="txt-color-secondary" />
+        )}
+      </div>
     </nav>
   );
 }
