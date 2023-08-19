778e84c5c4143a3711af718e0915bd6f42db1dbb
Valentina Otočan <valentinaotocan@gmail.com>
2023-08-19 21:16:12 +0200

    Add link and link style to NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,24 @@
+import { Link } from 'react-router-dom';
 import error from '../assets/images/icons/error-404.png';
 function NotFound() {
+  const linkStyle = {
+    color: "#0082c8",
+    textDecoration: "underline",
+    cursor: "pointer",
+  };
+
   return (
     <div className="error">
       <img src={error} alt="Error" />
       <p className="fs-xl">Uuuups, dogodila se greška!</p>
       <p>Server ne može pronaći traženo.</p>
+      <p>
+        Vrati se na{" "}
+        <Link to="/" style={linkStyle}>
+          početnu stranicu
+        </Link>
+        .
+      </p>
     </div>
   );
 }
