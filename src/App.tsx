6895ca671e3ac819a8d39cf302906130c7d974b3
Valentina Otočan <81707177+valentinaotocan@users.noreply.github.com>
2024-07-20 22:11:43 +0200

    Page transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Layout from "./components/Layout";
 import HomeLayout from "./components/HomeLayout";
 import Home from "./pages/Home";
@@ -7,22 +8,69 @@ import Experience from "./pages/Experience";
 import Project from "./pages/Project";
 import NotFound from "./pages/NotFound";
 import Contact from "./pages/Contact";
+import PageTransition from "./components/PageTransition";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <Routes>
-        <Route element={<HomeLayout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route element={<Layout />}>
-          <Route path="/o-meni" element={<About />} />
-          <Route path="/iskustvo" element={<Experience />} />
-          <Route path="/projekti" element={<Project />} />
-          <Route path="/kontakt" element={<Contact />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<HomeLayout />}>
+            <Route
+              index
+              element={
+                <PageTransition>
+                  <Home />
+                </PageTransition>
+              }
+            />
+          </Route>
+          <Route path="/" element={<Layout />}>
+            <Route
+              path="/o-meni"
+              element={
+                <PageTransition>
+                  <About />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/iskustvo"
+              element={
+                <PageTransition>
+                  <Experience />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/projekti"
+              element={
+                <PageTransition>
+                  <Project />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/kontakt"
+              element={
+                <PageTransition>
+                  <Contact />
+                </PageTransition>
+              }
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <PageTransition>
+                <NotFound />
+              </PageTransition>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
---
+++ src/components/PageTransition.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import { motion } from "framer-motion";
+import { pageVariants } from "../Animation";
+
+interface PageTransitionProps {
+  children: ReactNode;
+}
+
+const PageTransition: React.FC<PageTransitionProps> = ({ children }) => {
+  return (
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default PageTransition;
