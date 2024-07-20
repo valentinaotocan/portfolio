5fe9afe346d4607b3e447882b24fd991aaa5fb7d
Valentina Otočan <81707177+valentinaotocan@users.noreply.github.com>
2024-07-21 00:35:03 +0200

    Add translate for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
+import { useTranslation } from "react-i18next";
 import { NavLink } from 'react-router-dom';
 import { IoCloseSharp, IoReorderThreeSharp } from "react-icons/io5";
 
 function Header() {
   const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const { t, i18n } = useTranslation("global");
+
+  const onChangeLang = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const lang_code = e.target.value;
+    i18n.changeLanguage(lang_code);
+  };
 
   const toggleMenu = () => {
     setIsMenuOpened(!isMenuOpened);
@@ -14,27 +21,40 @@ function Header() {
     }
   };
 
+  const closeMenu = () => {
+    setIsMenuOpened(false);
+    document.body.classList.remove("menu-open");
+  };
+
+  const handleSelectClick = (
+    e: React.MouseEvent<HTMLSelectElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+  };
+
+
   return (
     <nav className="header flex flex-jc--fe flex-ai--c ptb-xl txt-color-secondary fw-semiBold sans-serif">
       <div
         className={`header__nav ${isMenuOpened ? "open" : ""}`}
         onClick={toggleMenu}
       >
-        <NavLink to="/" className="mr-xl underline">
-          Naslovna
+        <NavLink to="/" className="mr-xl underline" onClick={closeMenu}>
+          {t("header.home")}
         </NavLink>
-        <NavLink to="/o-meni" className="mr-xl underline">
-          O meni
+        <NavLink to="/o-meni" className="mr-xl underline" onClick={closeMenu}>
+          {t("header.about")}
         </NavLink>
-        <NavLink to="/iskustvo" className="mr-xl underline">
-          Iskustvo
+        <NavLink to="/projekti" className="mr-xl underline" onClick={closeMenu}>
+          {t("header.projects")}
         </NavLink>
-        <NavLink to="/projekti" className="mr-xl underline">
-          Projekti
+        <NavLink to="/kontakt" className="mr-xl underline" onClick={closeMenu}>
+          {t("header.contact")}
         </NavLink>
-        <NavLink to="/kontakt" className="mr-xl underline">
-          Kontakt
-        </NavLink>
+        <select onChange={onChangeLang} onClick={handleSelectClick}>
+          <option value="en">EN</option>
+          <option value="hr">HR</option>
+        </select>
       </div>
       <div className="header__hamburger" onClick={toggleMenu}>
         {isMenuOpened ? (
