f1bd51802a5dde459bb3fc243b32edfb0e99c2f8
Valentina Otočan <81707177+valentinaotocan@users.noreply.github.com>
2024-07-20 21:55:06 +0200

    Page transition

--- src/Animation.ts
+++ src/Animation.ts
@@ -28,3 +28,26 @@ export const fromTop = {
   },
 };
 
+export const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: "-100vh",
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: [0.46, 0.03, 0.52, 0.96],
+    },
+  },
+  out: {
+    opacity: 0,
+    y: "100vh",
+    transition: {
+      duration: 0.6,
+      ease: [0.46, 0.03, 0.52, 0.96],
+    },
+  },
+};
+
