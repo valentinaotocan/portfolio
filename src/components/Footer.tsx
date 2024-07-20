efecc99e4f911553e9cca238b4b379f9c5a7f351
Valentina Otočan <81707177+valentinaotocan@users.noreply.github.com>
2024-07-20 22:23:13 +0200

    Add translation and change spacing for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 export default function Footer() {
+  const { t } = useTranslation("global");
+  
   return (
-    <footer className="footer flex flex-jc--c flex-ai--c text-center linear-bg plr-custom txt-color-primary fs-base fw-normal">
+    <footer className="footer flex flex-jc--c flex-ai--c text-center linear-bg plr-custom txt-color-primary fs-base fw-normal ptb-xl">
       <p>
-        Sva prava pridržana. &copy; Valentina Otočan {new Date().getFullYear()}.
+        <span>{t('footer.rightsReserved')}</span> &copy; Valentina Otočan {new Date().getFullYear()}.
       </p>
     </footer>
   );
