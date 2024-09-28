db68cf308ba3261d5de41fa8d6f96a87fe0e7c84
Valentina Otočan <81707177+valentinaotocan@users.noreply.github.com>
2024-09-28 21:17:37 +0200

    Split SCSS for ProjectCard and tweaked styles

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,35 +2,38 @@ import { ProjectCardProps } from "../types";
 
 function ProjectCard({ post, t, formatDate }: ProjectCardProps) {
   return (
-    <div
-      className="projects__cards__card flex flex-fd--c flex-jc--sb"
-    >
-      <div>
-        <h2 className="shadow">
+    <div className="card">
+      <div className="card__project-name">
+        <h2>
           {t("projects.title")}: {post.name}
         </h2>
-        <p className="ellipsis ellipsis--3">
-          <span className="fw-semiBold">{t("projects.description")}: </span>
-          {post.description !== null ? post.description : "/"}
-        </p>
-        <p className="ellipsis">
-          <span className="fw-semiBold">{t("projects.tags")}: </span>
-          {post.topics.length > 0 ? post.topics.join(", ") : "/"}
-        </p>
-        <p>
-          <span className="fw-semiBold">{t("projects.prevalent")}: </span>
-          {post.language}
-        </p>
-        <p>
-          <span className="fw-semiBold">{t("projects.created")}: </span>{" "}
-          {formatDate(post.created_at)}
-        </p>
       </div>
-      <a href={post.html_url}>
-        <button type="button" className="btn linear-bg sans-serif2">
-          {t("projects.more")}
-        </button>
-      </a>
+
+      <div className="card__project-content flex flex-fd--c flex-jc--sb">
+        <div>
+          <p className="ellipsis ellipsis--3">
+            <span className="fw-semiBold">{t("projects.description")}: </span>
+            {post.description !== null ? post.description : "/"}
+          </p>
+          <p className="ellipsis">
+            <span className="fw-semiBold">{t("projects.tags")}: </span>
+            {post.topics.length > 0 ? post.topics.join(", ") : "/"}
+          </p>
+          <p>
+            <span className="fw-semiBold">{t("projects.prevalent")}: </span>
+            {post.language}
+          </p>
+          <p>
+            <span className="fw-semiBold">{t("projects.created")}: </span>{" "}
+            {formatDate(post.created_at)}
+          </p>
+        </div>
+        <a href={post.html_url}>
+          <button type="button" className="btn linear-bg sans-serif2">
+            {t("projects.more")}
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
